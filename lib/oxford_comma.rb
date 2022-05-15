514732a85194f1b08d7f8561490bbf9f1e41d8eb
Matthew <mattj20@gmail.com>
2022-05-14 18:58:37 -0700

    commit tree 7adcb76039f51891270e72f64e64729a7d5c4062

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
-
+    if array.size > 1 && array.size <= 2
+        array.join(" and ")
+    elsif array.size == 1
+        array.join
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
